9506f80051aa7ad229a4636a74a5f70b46ba7bbf
Renan Barroso <barrosoren@gmail.com>
2022-11-06 19:11:49 +0200

    add engine

---
+++ src/engine.rs
@@ -0,0 +1,34 @@
+// Graphics
+pub fn clear_background() {
+    macroquad::prelude::clear_background(macroquad::prelude::GRAY);
+}
+
+pub fn draw_circle(x: f32, y: f32, r: f32) {
+    macroquad::prelude::draw_circle_lines(x, y_transform(y), r, 2.0, macroquad::prelude::BLUE);
+}
+
+fn y_transform(y: f32) -> f32 {
+    (macroquad::prelude::screen_height() as f32) - y
+}
+
+// Time
+pub async fn await_next_frame() {
+    macroquad::prelude::next_frame().await
+}
+
+// Input
+pub struct MoveKeys {
+    pub up: bool,
+    pub down: bool,
+    pub left: bool,
+    pub right: bool,
+}
+
+pub fn get_active_move_keys() -> MoveKeys {
+    MoveKeys {
+        up: macroquad::prelude::is_key_down(macroquad::prelude::KeyCode::W),
+        down: macroquad::prelude::is_key_down(macroquad::prelude::KeyCode::S),
+        left: macroquad::prelude::is_key_down(macroquad::prelude::KeyCode::A),
+        right: macroquad::prelude::is_key_down(macroquad::prelude::KeyCode::D),
+    }
+}
