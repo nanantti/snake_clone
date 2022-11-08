c855f04bf50b8f5887ac3202ce5e4a5f885b8fd6
Renan Barroso <barrosoren@gmail.com>
2022-11-08 21:18:41 +0200

    reduce input delay

--- src/main.rs
+++ src/main.rs
@@ -22,21 +22,24 @@ fn draw_player(pl: &player::Player, gd: &grid::Grid) {
 
 #[macroquad::main("Snake")]
 async fn main() {
-    const FRAME_DURATION_SECONDS: f64 = 1.0 / 10.0;
+    const STEP_DURATION_SECONDS: f64 = 1.0 / 10.0;
     let n_cells = (24, 24);
     let mut grid = grid::Grid {
         number_of_cells: &n_cells,
         screen_size: (engine::get_screen_width(), engine::get_screen_height()),
     };
     let mut player = player::Player::new((12, 12), &n_cells);
+    let mut last_step_time = engine::get_time();
     loop {
-        let timestamp_start_frame = engine::get_time();
         grid.update_screen_size((engine::get_screen_width(), engine::get_screen_height()));
         engine::clear_background();
-        player.update(&engine::get_active_move_keys());
         draw_player(&player, &grid);
 
-        while engine::get_time() - timestamp_start_frame < FRAME_DURATION_SECONDS {}
+        let current_time = engine::get_time();
+        if current_time - last_step_time > STEP_DURATION_SECONDS {
+            player.update(&engine::get_active_move_keys());
+            last_step_time = current_time;
+        }
         engine::await_next_frame().await
     }
 }
