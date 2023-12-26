2ccc605b7e1411321dea92583259917fede522fb
Renan Barroso <barrosoren@gmail.com>
2023-12-26 19:07:56 +0200

    remove build.sh

--- build.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-cargo build --target wasm32-unknown-unknown --release
-#wasm-bindgen target/wasm32-unknown-unknown/release/snake.wasm --out-dir ./wasm_help --no-modules --no-typescript
-wasm-bindgen target/wasm32-unknown-unknown/release/snake.wasm --out-dir ./wasm_help --no-typescript
